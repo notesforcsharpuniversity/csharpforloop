3edf730794a5dfdfab9cb407ca023c05609ac5b1
Ali Osman Mollahüseyinoğlu <aliosman.m@hotmail.com>
2014-10-15 20:40:48 +0300

    c sharp for loop
    for loop example

---
+++ loopProject/loopProject/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loopProject
+{
+    public partial class Form1 : Form
+    {
+        string[] yazi = { "a", "b", "c", "d", "e" };
+        int[] sayi = { 1, 2, 3, 4, 5 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void doldur(string[] a1, int[] a2) 
+        {
+            for (int i = 0; i < a1.Length; i++)
+            {
+                for (int y = 0; y < a2.Length; y++)
+                {
+                    listBox1.Items.Add(a1[i] + " / " + a2[y]);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            doldur(yazi, sayi);
+        }
+    }
+}
